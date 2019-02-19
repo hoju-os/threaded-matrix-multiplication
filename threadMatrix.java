acbc3c33bb872c88e33b700b9ae109b2205d2841
hoju-os <47367882+hoju-os@users.noreply.github.com>
2019-02-18 19:24:07 -0500

    Update threadMatrix.java

--- threadMatrix.java
+++ threadMatrix.java
@@ -14,40 +14,69 @@ public class threadMatrix extends Thread {
 
     public void run ()
     {
+
         //matrixThree[i][j] = (matrixOne[i][0] * matrixTwo[0][j])+(matrixOne[i][1] * matrixTwo[1][j])+(matrixOne[i][2] * matrixTwo[2][j]);
-    	for(int k = 0; k <= matrixThree[0].length; k++)
-    	{
-    		product += (matrixOne[i][k] * matrixTwo[k][j]);
-    	}
-    	matrixThree[i][j] = product;
+        for(int k = 0; k <= matrixOne[0].length-1; k++)
+        {
+            product += (matrixOne[i][k] * matrixTwo[k][j]);
+        }
+        matrixThree[i][j] = product;
+
     }
 
     public static void main(String[] args) throws InterruptedException {
-        
 
-    	int [][] matrixOne = {
-                {3,7,6,6,6},
-                
+
+        int [][] matrixOne = {
+                {3,7,6,6,6,3,3},
+                {3,7,6,6,6,3,3},
+                {3,7,6,6,6,3,3}
+
         };
         int [][] matrixTwo = {
                 {3,7,2,5},
                 {3,2,9,9},
                 {3,2,9,3},
                 {3,7,2,7},
+                {3,2,9,3},
+                {3,7,2,7},
                 {3,7,2,7}
-                
+
         };
-               
-        
+
+
         int n = matrixOne.length;
         int m = matrixOne[0].length;
         int k = matrixTwo[0].length;
 
+
         int numOfThreads = n * k;
-        
+
         int [][] matrixThree = new int [n][k];
 
         Thread [][] threads = new Thread[n][k];
+
+        System.out.println("Matrix A\n");
+        for (int i = 0; i < matrixOne.length; i++)
+        {
+            for(int j = 0; j < matrixOne[0].length; j++)
+            {
+                System.out.print(matrixOne[i][j] + "\t");
+            }
+            System.out.println();
+        }
+        System.out.println("\n\n");
+
+        System.out.println("Matrix B\n");
+        for (int i = 0; i < matrixTwo.length; i++)
+        {
+            for(int j = 0; j < matrixTwo[0].length; j++)
+            {
+                System.out.print(matrixTwo[i][j] + "\t");
+            }
+            System.out.println();
+        }
+        System.out.println("\n\n");
 
         for (int i = 0; i <= n-1; i++)
         {
@@ -65,12 +94,15 @@ public class threadMatrix extends Thread {
             }
         }
 
+        System.out.println("Matrix C\n");
         for (int i = 0; i < matrixThree.length; i++) {
             for (int j = 0; j < matrixThree[i].length; j++) {
-                System.out.print(matrixThree[i][j] + " ");
+                System.out.print(matrixThree[i][j] + "\t");
             }
             System.out.println();
         }
+
+
 
     }
 }
